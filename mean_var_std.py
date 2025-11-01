5595964ae00e48793a16d60d3449490786f0673a
lpm <leodanypm@gmail.com>
2025-10-31 21:28:24 -0400

    Coreccion

--- mean_var_std.py
+++ mean_var_std.py
@@ -23,5 +23,4 @@ def calculate(list):
              'sum': [array.sum(axis=0).tolist(), array.sum(axis=1).tolist(), float(array.sum())] }
 
     return calculations
-print(calculate([0,1,2,3,4,5,6,7,8]))
 
